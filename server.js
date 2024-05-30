a98f2363a94ecd82b4bad9691e920dbd4f70554e
Supochai <supholchai.po@gmail.com>
2024-05-30 18:54:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const app = express()
-var corsOptions = { origin:'*'};
-//const {db} = require("./app/models"); //sync database
-//db.sequelize.sync();
-app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('trust proxy', true);
 require("dotenv").config()
 let port = process.env.SERVER_PORT
 app.listen(port, () => {
