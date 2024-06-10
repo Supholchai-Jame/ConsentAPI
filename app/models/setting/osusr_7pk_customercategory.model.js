65e745f8cdaafc20ab7cca4ae3f53aae71e98dea
Supochai <supholchai.po@gmail.com>
2024-06-10 08:54:46 +0700

    update

--- app/models/setting/osusr_7pk_customercategory.model.js
+++ app/models/setting/osusr_7pk_customercategory.model.js
@@ -1,6 +1,21 @@
+const { DELETE } = require("sequelize/lib/query-types");
+
 module.exports = (sequelize, Sequelize) => {
     const settingsVehicleBrand = sequelize.define("OSUSR_7PK_CUSTOMERCATEGORY", {
       ID: {type: Sequelize.INTEGER,primaryKey: true, autoIncrement: true },
+      ADDCUSTOMERCATEGORYID : {type: Sequelize.BIGINT},
+      CONSENTID : {type: Sequelize.BIGINT},
+      ISACTIVE  : {type: Sequelize.BOOLEAN},
+      ISDELETED: {type: Sequelize.BOOLEAN},
+      CREATORID: {type: Sequelize.BIGINT},
+      CREATORNAME: {type: Sequelize.STRING(250)},
+      CREATEDDATE: {type: Sequelize.DATE},
+      MODIFIEDID: {type: Sequelize.BIGINT},
+      MODIFIEDNAME: {type: Sequelize.STRING(250)},
+      MODIFIEDDATE: {type: Sequelize.DATE},
+      DELETEDID: {type: Sequelize.BIGINT},
+      DELETEDNAME: {type: Sequelize.STRING(250)},
+      DELETEDDATE: {type: Sequelize.DATE}
     }, { timestamps: false });
     return settingsVehicleBrand;
 };
